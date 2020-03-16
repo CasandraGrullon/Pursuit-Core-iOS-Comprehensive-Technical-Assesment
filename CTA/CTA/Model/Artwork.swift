c11f87af11f71438e02e750adf3cf4d35b149b4b
Casandra <casandragrullon@pursuit.org>
2020-03-16 12:53:22 -0400

    added museum models for collection and artwork

---
+++ CTA/CTA/Model/Artwork.swift
@@ -0,0 +1,30 @@
+//
+//  Artwork.swift
+//  CTA
+//
+//  Created by casandra grullon on 3/16/20.
+//  Copyright © 2020 casandra grullon. All rights reserved.
+//
+
+import Foundation
+
+struct Artwork: Codable {
+    let id: String
+    let title: String
+    let webImage: ArtImage
+    let titles: [String]
+    let description: String
+    let principalMakers: PrincipalMakers
+    let physicalMedium: String
+    let subTitle: String
+}
+struct ArtImage: Codable {
+    let url: String
+}
+struct PrincipalMakers: Codable {
+    let name: String
+    let placeOfBirth: String
+    let dateOfBirth: String
+    let dateOfDeath: String
+    let placeOfDeath: String
+}
---
+++ CTA/CTA/Model/Collection.swift
@@ -0,0 +1,36 @@
+//
+//  RijkMuseum.swift
+//  CTA
+//
+//  Created by casandra grullon on 3/16/20.
+//  Copyright © 2020 casandra grullon. All rights reserved.
+//
+
+import Foundation
+
+struct Collection: Codable {
+    let artObjects: [ArtObjects]
+}
+struct ArtObjects: Codable {
+    let links: Links
+    let id: String
+    let title: String
+    let artist: String//coding key
+    let longTitle: String
+    let webImage: WebImage
+    
+    enum CodingKeys: String, CodingKey {
+        case links
+        case id
+        case title
+        case artist = "principalOrFirstMaker"
+        case longTitle
+        case webImage
+    }
+}
+struct Links: Codable {
+    let web: String
+}
+struct WebImage: Codable {
+    let url: String
+}
