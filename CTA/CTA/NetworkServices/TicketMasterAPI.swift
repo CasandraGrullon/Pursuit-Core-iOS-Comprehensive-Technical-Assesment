0829d0a9a9a0d9e2ff8630af85206ad902aba6f1
Casandra <casandragrullon@pursuit.org>
2020-03-16 13:18:37 -0400

    api call for ticketmaster made

--- CTA/CTA/NetworkServices/TicketMasterAPI.swift
+++ CTA/CTA/NetworkServices/TicketMasterAPI.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import NetworkHelper
+
+class TicketMasterAPI {
+    static func getEvents(postalCode: String, completion: @escaping (Result<[Events], AppError>) -> () ) {
+        let endpointURL = "https://app.ticketmaster.com/discovery/v2/events.json?apikey=iYY6FrsDrhmcnXiLE9EvGCN9fA5mW775&postalCode=\(postalCode)"
+        guard let url = URL(string: endpointURL) else {
+            completion(.failure(.badURL(endpointURL)))
+            return
+        }
+        let request = URLRequest(url: url)
+        
+        NetworkHelper.shared.performDataTask(with: request) { (result) in
+            switch result {
+            case .failure(let appError):
+                completion(.failure(.networkClientError(appError)))
+            case .success(let data):
+                do {
+                    let events = try JSONDecoder().decode(EventsResults.self, from: data)
+                    completion(.success(events.events))
+                } catch {
+                    completion(.failure(.decodingError(error)))
+                }
+            }
+        }
+    }
+}
